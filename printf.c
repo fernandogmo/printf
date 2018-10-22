1a45fa95d00eb49b2fff80f486b3a33ccd49d896
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-22 16:28:11 +0000

    Handles c, s and % with our latest changes. Includes small test in main function. Meant to close #6 and close #8.

--- printf.c
+++ printf.c
@@ -1,31 +1,33 @@
+#include <stdio.h> /* for testing only */
 #include <stdarg.h> /* va_list, va_start, va_arg, va_end macros */
 #include <stdlib.h> /* NULL macro */
 #include "holberton.h" /* _putchar */
 
-
-
-/**
- * struct printer - format printer struct
- * @spec: the format specifier
- * @fn: the function that handles spec
- */
-typedef struct printer
-{
-	char *spec;
-	void (*fn) (va_list);
-} print_t;
-
-
-
 /**
  * print_char - prints a char parameter from a va_list
  * @ap: va_list from calling function
+ * Return: integer count of characters printed
  */
-void print_char(va_list ap)
+int print_char(va_list ap)
 {
-	_putchar(va_arg(ap, int));
+	return (_putchar(va_arg(ap, int)));
 }
 
+/**
+ * print_string - prints a string parameter from a va_list
+ * @ap: va_list from calling function
+ * Return: integer count of characters printed
+ */
+int print_string(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	int count = 0;
+
+	while (str[count] != '\0')
+		count += _putchar(str[count]);
+
+	return (count);
+}
 
 
 /**
@@ -35,15 +37,66 @@ void print_char(va_list ap)
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	int i, j, count = 0;
+	va_list ap;
+	print_t funcs[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
 
-	/* begin filler code */
-	if (format != NULL)
+	va_start(ap, format);
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
-		_putchar(*format);
-		count++;
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			continue;
+		}
+		switch (format[++i])
+		{
+		case '%':
+			count += _putchar('%');
+			break;
+		case 'c':
+		case 's':
+			for (j = 0; funcs[j].spec != NULL; j++)
+			{
+				if (format[i] == funcs[j].spec[0])
+				{
+					count += funcs[j].fn(ap);
+					break;
+				}
+			}
+			break;
+		default:
+			exit(-1);
+		}
 	}
-	/* end filler code */
-
+	va_end(ap);
 	return (count);
+}
+
+/* TODO: REMOVE BEFORE PUSHING TO MASTER! */
+/**
+ * main - tests _printf against stdio::printf
+ * Return: 0 on SUCCESS
+ */
+int main(void)
+{
+	int a, b;
+
+	a = printf("a\n");
+	b = _printf("a\n");
+	printf("(%d, %d)\n", a, b);
+
+	a = printf("%c\n", 'A');
+	b = _printf("%c\n", 'A');
+	printf("(%d, %d)\n", a, b);
+
+	a = printf("%s\n", "Holberton");
+	b = _printf("%s\n", "Holberton");
+	printf("(%d, %d)\n", a, b);
+
+	return (0);
 }
