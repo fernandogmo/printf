956adecc6b135348349e4fc12f077f74bd29bf32
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-10-22 00:03:29 +0000

    Included stdarg.h, implemented printer struct & print_char.

--- printf.c
+++ printf.c
@@ -1,5 +1,32 @@
+#include <stdarg.h> /* va_list, va_start, va_arg, va_end macros */
 #include <stdlib.h> /* NULL macro */
 #include "holberton.h" /* _putchar */
+
+
+
+/**
+ * struct printer - format printer struct
+ * @spec: the format specifier
+ * @fn: the function that handles spec
+ */
+typedef struct printer
+{
+	char *spec;
+	void (*fn) (va_list);
+} print_t;
+
+
+
+/**
+ * print_char - prints a char parameter from a va_list
+ * @ap: va_list from calling function
+ */
+void print_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+}
+
+
 
 /**
  * _printf - prints to stdout according to a format string
